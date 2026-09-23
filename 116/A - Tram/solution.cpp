745b9d7ca0a2d024d1354f0669e730d2c71ac1dd
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-09-23 09:48:35 +0530

    Add Tram [A] from Codeforces

---
+++ 116/A - Tram/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    int current = 0;
+    int maximum = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+ 
+        current -= a;  // passengers exit
+        current += b;  // passengers enter
+ 
+        maximum = max(maximum, current);
+    }
+ 
+    cout << maximum << endl;
+ 
+    return 0;
+}
